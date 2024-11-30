90c5e3d5d9823675e4e37f05b683d6624789e013
Pratiksha Sapkale <pratikshasapkale495@gmail.com>
2024-11-30 14:42:12 +0530

    Add files via upload

---
+++ CircleAreaCalculator.java
@@ -0,0 +1,17 @@
+package com.example.geometry;
+
+public class CircleAreaCalculator {
+
+	public static void main(String[] args) {
+		
+		 double radius = 4;
+
+	        // Calculating area of the circle
+	        double area = 3.14159* radius * radius;
+
+	        // Printing the result
+	        System.out.println("Area of the circle with radius " + radius + " is: " + area);
+
+	}
+
+}
